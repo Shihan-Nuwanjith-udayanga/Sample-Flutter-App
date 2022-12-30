c0e27204970dc7c63a552eb30bbfd51f35d3212c
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-12-30 08:28:42 +0530

    all

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ui_design_class/login_page.dart';
+import 'package:ui_design_class/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const SplashScreen(),
     );
   }
 }
