e16223faad805f2b0d0375501fb3daf8be82a7a5
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-01-15 08:14:04 +0530

    create a new home page

--- lib/new_home_page.dart
+++ lib/new_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ui_design_class/rest_api_service.dart';
+import 'package:ui_design_class/user_screen.dart';
 
 class NewHomePage extends StatefulWidget {
   const NewHomePage({Key? key}) : super(key: key);
@@ -19,6 +20,8 @@ class _NewHomePageState extends State<NewHomePage> {
     super.initState();
 
     apiService.getUsers();
+
+
   }
 
   @override
@@ -27,7 +30,67 @@ class _NewHomePageState extends State<NewHomePage> {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: FutureBuilder(
+              future: apiService.getUsers(),
+              builder: (context , snapShot){
+                if(snapShot.hasData){
+                  return ListView.builder(
+                    // itemCount: snapShot.data.length,
+                    itemBuilder: (context , index){
+                      // return Container( margin: EdgeInsets.all(8), height:50 , width: 200, color: Colors.blue);
+                      return InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => UserScreen(
+                            user: snapShot.data![index],
+                          )));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 3,
+                                spreadRadius: 3,
+                                color: Colors.grey.withOpacity(0.3)
+                              )
+                            ]
+                          ),
+                          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                          child: ListTile(
+                            title: Text(snapShot.data![index].name ?? '', style: TextStyle(fontSize: 18, color: Colors.black),),
+                            subtitle: Text(snapShot.data![index].city ?? '', style: TextStyle(fontSize: 18, color: Colors.black),),
+                            leading: ClipOval(
+                              child: Image.network(snapShot.data![index].image, fit: BoxFit.cover, width: 60, height: 60,),
+                            ),
+                            trailing: IconButton(
+                              icon: Icon(Icons.favorite_border),
+                              onPressed: () {  } ,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }else{
+                  return Container(
+                    child: Center(
+                      child: Text('Loading...', style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                      )),
+                    ),
+                  );
+                }
+              },
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
